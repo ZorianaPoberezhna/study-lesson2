98ae28784c44bab084ddd77511cd2cdb3ec65335
Zoriana <zorianapoberezhna0608@gmail.com>
2024-09-17 20:55:24 +0300

    test commit

---
+++ module.py
@@ -0,0 +1,65 @@
+#import re
+
+
+#text = input("Text:")
+#if re.match(r"^[a-zA-Z]+$", text ):
+#    print("Valid.")
+#else:
+#    print("Invalid.")
+#exc2
+#import re
+
+
+#numbers = input("Enter string of numbers:")
+#if re.match(r"^[0-9]+$", numbers ):
+#    print("Valid.")
+#else:
+#    print("Invalid.")
+#exc3
+#import re
+
+
+#text = input("Enter zip codes:")
+#zip_codes = re.findall(r"\b\d{5}\b", text)
+#print(zip_codes)
+#exc4
+
+
+#import re
+
+
+#date = input("Enter text:")
+#if re.match(r'^\d{2}-\d{2}-\d{4}$', date):
+#    print("Valid")
+#else:
+#    print("Invalid")
+#exc5
+#import re
+
+
+#number = input("Enter your number:")
+#if re.match(r'^\+\d{3} \d{2} \d{3}-\d{4}$', number):
+#    print("Valid")
+#else:
+#    print("Invalid")
+
+
+#exc6
+#import re
+
+#email = input("Enter email:")
+#if re.match(r'^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$', email):
+#    print("Valid")
+#else:
+#    print("Invalid")
+#exc7
+#import re
+
+
+#ip_address = input("Enter IP-address:")
+#if re.match(r'((25[0-5]|2[0-4][]0-9]|1[0-9]{2}|[1-9]?[0-9])\.)'
+#            r'{3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}'
+#            r'|[1-9]?[0-9])$',ip_address):
+#    print("Valid")
+#else:
+#    print("Invalid")
