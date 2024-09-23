bc5e9b5a4b6aa34eda88fa97e0b87ec9f3e1774a
Zoriana <zorianapoberezhna0608@gmail.com>
2024-09-23 08:50:31 +0300

    task5

---
+++ excep6.py
@@ -0,0 +1,11 @@
+def file_recording(file1: str):
+
+
+    user_input = input("Enter text:")
+
+    with open(file1, 'w') as file:
+        file.write(user_input)
+    print(f"Your text in {file1}")
+
+
+file_recording("file1.txt")
